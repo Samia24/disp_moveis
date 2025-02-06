d6fa1fdc6ea59da553b07674b8d7b24c272ba59f
Francisco David Santos Sousa <franciscodavidsantossousa@gmail.com>
2025-02-05 22:23:10 -0300

    FEAT: add firebase conexao  e outras modificacoes v1

---
+++ lib/controllers/cadastro_controllers.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import '../model/banco.dart';
+
+class CadastroController {
+  final TextEditingController nomeController = TextEditingController();
+  final TextEditingController cpfController = TextEditingController();
+  final TextEditingController cnpjController = TextEditingController();
+  final TextEditingController telefoneController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController senhaController = TextEditingController();
+  final TextEditingController enderecoController = TextEditingController();
+  final TextEditingController bairroController = TextEditingController();
+  final TextEditingController complementoController = TextEditingController();
+  final TextEditingController cepController = TextEditingController();
+  final TextEditingController cidadeController = TextEditingController();
+  final TextEditingController estadoController = TextEditingController();
+  final TextEditingController latitudeController = TextEditingController();
+  final TextEditingController longitudeController = TextEditingController();
+
+  final Banco _banco = Banco();
+
+  Future<void> cadastrarUsuario(BuildContext context) async {
+    String nome = nomeController.text;
+    String cpf = nomeController.text;
+    String cnpj = nomeController.text;
+    String telefone = telefoneController.text;
+    String email = emailController.text;
+    String senha = senhaController.text;
+    String endereco = enderecoController.text;
+    String bairro = bairroController.text;
+    String complemento = complementoController.text;
+    String cep = cepController.text;
+    String cidade = cidadeController.text;
+    String estado = estadoController.text;
+    String latitude = latitudeController.text;
+    String longitude = longitudeController.text;
+
+    if (nome.isNotEmpty &&
+        cpf.isNotEmpty &&
+        cnpj.isNotEmpty &&
+        telefone.isNotEmpty &&
+        email.isNotEmpty &&
+        senha.isNotEmpty &&
+        endereco.isNotEmpty &&
+        bairro.isNotEmpty &&
+        complemento.isNotEmpty &&
+        cep.isNotEmpty &&
+        cidade.isNotEmpty &&
+        estado.isNotEmpty &&
+        latitude.isNotEmpty &&
+        longitude.isNotEmpty) {
+      try {
+        await _banco.salvarContato(
+            nome,
+            cpf,
+            cnpj,
+            telefone,
+            email,
+            senha,
+            endereco,
+            bairro,
+            complemento,
+            cep,
+            cidade,
+            estado,
+            latitude,
+            longitude);
+
+        Navigator.pop(context);
+      } catch (e) {
+        print("Erro ao cadastrar: $e");
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Erro ao cadastrar: $e"),
+          ),
+        );
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Preencha todos os campos!"),
+        ),
+      );
+    }
+  }
+}
--- lib/views/cadastro_usuario.dart
+++ lib/views/cadastro_usuario.dart
@@ -7,9 +7,9 @@ class CadastroUsuario extends StatefulWidget {
   State<CadastroUsuario> createState() => _CadastroUsuarioState();
 }
 
-class _CadastroUsuarioState extends State<CadastroUsuario> {
-  bool usuarioAtivado = true; // true para pessoa, false para loja
+bool usuarioAtivado = true; // true para pessoa, false para loja
 
+class _CadastroUsuarioState extends State<CadastroUsuario> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -36,7 +36,9 @@ class _CadastroUsuarioState extends State<CadastroUsuario> {
               tabs: [
                 const Tab(icon: Icon(Icons.person), text: "Contato"),
                 const Tab(icon: Icon(Icons.location_on), text: "Endereço"),
-                usuarioAtivado ? Tab(icon: Icon(Icons.payment), text: "Pagamento") : Tab(icon: Icon(Icons.pix), text: "PIX") ,
+                usuarioAtivado
+                    ? Tab(icon: Icon(Icons.payment), text: "Pagamento")
+                    : Tab(icon: Icon(Icons.pix), text: "PIX"),
               ],
             ),
 
@@ -111,7 +113,8 @@ class _CadastroUsuarioState extends State<CadastroUsuario> {
         child: Column(
           children: [
             TextFormField(
-              decoration: _inputDecoration("Nome completo", "Informe seu nome completo"),
+              decoration: _inputDecoration(
+                  "Nome completo", "Informe seu nome completo"),
             ),
             const SizedBox(height: 10),
             usuarioAtivado
@@ -129,6 +132,10 @@ class _CadastroUsuarioState extends State<CadastroUsuario> {
             TextFormField(
               decoration: _inputDecoration("E-mail", "email@gmail.com"),
             ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: _inputDecoration("Senha", "Senha de 4 dígitos"),
+            ),
           ],
         ),
       ),
@@ -141,63 +148,41 @@ class _CadastroUsuarioState extends State<CadastroUsuario> {
         padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 20),
         child: Column(
           children: [
-            if (usuarioAtivado)...[
-              TextFormField(
-                decoration: _inputDecoration("Endereço", "Informe seu endereço"),
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: _inputDecoration("Bairro", "Informe seu bairro"),
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: _inputDecoration("Complemento", "Informe o complemento"),
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: _inputDecoration("CEP", "xxxxx-xxx"),
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: _inputDecoration("Cidade", "Informe sua cidade"),
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: _inputDecoration("Estado", "Informe a sigla do seu estado"),
-              ),
-            ] else...[              
-              TextFormField(
-                decoration: _inputDecoration("Endereço", "Informe seu endereço"),
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: _inputDecoration("Bairro", "Informe seu bairro"),
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: _inputDecoration("Complemento", "Informe o complemento"),
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: _inputDecoration("CEP", "xxxxx-xxx"),
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: _inputDecoration("Cidade", "Informe sua cidade"),
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: _inputDecoration("Estado", "Informe a sigla do seu estado"),
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: _inputDecoration("Latitude", "Informe a latitude de sua loja"),
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: _inputDecoration("Longitude", "Informe a longitude de sua loja"),
-              ),
-            ],
+            TextFormField(
+              decoration: _inputDecoration("Endereço", "Informe seu endereço"),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: _inputDecoration("Bairro", "Informe seu bairro"),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration:
+                  _inputDecoration("Complemento", "Informe o complemento"),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: _inputDecoration("CEP", "xxxxx-xxx"),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: _inputDecoration("Cidade", "Informe sua cidade"),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration:
+                  _inputDecoration("Estado", "Informe a sigla do seu estado"),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: _inputDecoration(
+                  "Latitude", "Informe a latitude de sua loja"),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: _inputDecoration(
+                  "Longitude", "Informe a longitude de sua loja"),
+            ),
           ],
         ),
       ),
@@ -211,13 +196,14 @@ class _CadastroUsuarioState extends State<CadastroUsuario> {
         child: Column(
           children: [
             Text(
-              "Adicionar Cartão de Crédito/Débito", 
-              style: TextStyle(fontSize: 18, color: const Color.fromARGB(255, 18, 9, 8)),
+              "Adicionar Cartão de Crédito/Débito",
+              style: TextStyle(
+                  fontSize: 18, color: const Color.fromARGB(255, 18, 9, 8)),
             ),
             const SizedBox(height: 20),
-
             TextFormField(
-              decoration: _inputDecoration("Cartão de Crédito", "Número do cartão"),
+              decoration:
+                  _inputDecoration("Cartão de Crédito", "Número do cartão"),
             ),
             const SizedBox(height: 10),
             TextFormField(
@@ -262,10 +248,8 @@ class _CadastroUsuarioState extends State<CadastroUsuario> {
               style: TextStyle(fontSize: 20, color: Colors.red),
             ),
             const SizedBox(height: 10),
-            
             Icon(Icons.qr_code, size: 100, color: Colors.red),
             const SizedBox(height: 10),
-
             ElevatedButton(
               onPressed: () {},
               style: ElevatedButton.styleFrom(
@@ -276,7 +260,8 @@ class _CadastroUsuarioState extends State<CadastroUsuario> {
             ),
             const SizedBox(height: 20),
             TextFormField(
-              decoration: _inputDecoration("Código PIX copia e cola", "Informe o código"),
+              decoration: _inputDecoration(
+                  "Código PIX copia e cola", "Informe o código"),
             ),
             const SizedBox(height: 20),
             SizedBox(
